38230c117fdeffb88670f9cfe8c69e99b4d6c630
boadita <sboadam@gmail.com>
2025-03-22 15:23:27 -0400

    Correccion de vite config y otros

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Usa "./" si el problema persiste
+  base: "/", // Usa "./" si el problema persiste
+  build: {
+    outDir: "dist"
+  }
 })
