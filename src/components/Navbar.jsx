9880ada7013e4f54d8acfe294bade7c5a366601a
boadita <sboadam@gmail.com>
2026-03-18 00:44:33 -0400

    menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,16 @@ const Navbar = () => {
   const { darkMode, setDarkMode } = useContext(ThemeContext);
 
   return (
-    <nav className={`p-4 ${darkMode ? "bg-black text-white font-bold" : "bg-gray text-black font-bold"}`}>
-      <ul className="flex justify-center space-x-6">
-      <li><a href="/">Inicio</a></li>
+    <nav className={`p-4 ${darkMode ? "bg-black text-white font-bold w-full" : "bg-gray text-black font-bold w-full"}`}>
+     <div className="max-w-screen-xl mx-auto px-4 py-4">      
+      <ul className="flex flex-wrap justify-center gap-6">
+        <li><a href="/">Inicio</a></li>
         <li><a href="/about">Sobre mí</a></li>
         <li><a href="/projects">Proyectos</a></li>
         <li><a href="/skills">Habilidades</a></li>
         <li><a href="/contact-form">Contacto</a></li>
       </ul>
+      </div>
       <button 
         onClick={() => setDarkMode(!darkMode)} 
         className="ml-4 p-2 border rounded"
